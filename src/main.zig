216a3516df857e88e73707aa37243ddf07e7aad3
Hugo Bouderlique <bouderliqueh@gmail.com>
2024-02-07 21:01:48 +1100

    experiment with entering raw mode

--- src/main.zig
+++ src/main.zig
@@ -1,17 +1,46 @@
 const std = @import("std");
+const fs = std.fs;
+const io = std.io;
+const os = std.os;
+
+fn enter_canonical_mode(tty: fs.File, original_termios: os.termios) !void {
+    // const stdout = io.getStdOut().writer();
+    try os.tcsetattr(tty.handle, .FLUSH, original_termios);
+    // try stdout.writeAll("\x1B[?1049l"); // Disable alternative buffer.
+    // try stdout.writeAll("\x1B[?47l"); // Restore screen.
+    // try stdout.writeAll("\x1B[u"); // Restore cursor position.
+}
+
+fn enter_raw_mode(tty: fs.File, original_termios: os.termios) !void {
+    // const stdout = io.getStdOut().writer();
+
+    var raw = original_termios;
+    raw.cc[os.system.V.TIME] = 0;
+    raw.cc[os.system.V.MIN] = 1;
+
+    try os.tcsetattr(tty.handle, .FLUSH, raw);
+    // try stdout.writeAll("\x1B[?25l"); // Hide the cursor.
+    // try stdout.writeAll("\x1B[s"); // Save cursor position.
+    // try stdout.writeAll("\x1B[?47h"); // Save screen.
+    // try stdout.writeAll("\x1B[?1049h"); // Enable alternative buffer.
+}
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.stdout.print("All your {s} are belong to us.\n", .{"codebase"});
+    const tty: fs.File = try fs.cwd().openFile("/dev/tty", .{ .mode = fs.File.OpenMode.read_write });
+    defer tty.close();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    const original_termios = try os.tcgetattr(tty.handle);
+    try enter_raw_mode(tty, original_termios);
+    defer enter_canonical_mode(tty, original_termios) catch {};
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    const stdout = io.getStdOut().writer();
+    const stdin = io.getStdIn().reader();
+    var buffer: [1]u8 = undefined;
 
-    try bw.flush(); // don't forget to flush!
+    _ = try stdin.read(&buffer);
+
+    while (buffer[0] != 'q') {
+        try stdout.print("user input > {c}\n", .{buffer[0]});
+        _ = try stdin.read(&buffer);
+    }
 }
