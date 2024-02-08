b07073a3877c647f61bf750e0f1de857c91f6767
Hugo Bouderlique <bouderliqueh@gmail.com>
2024-02-08 17:59:03 +1100

    finally enter raw mode

--- src/main.zig
+++ src/main.zig
@@ -10,32 +10,44 @@ const c = @cImport({
 
 /// # Original Termios struct
 /// Used to reset terminal on exit
-const original_termios: c.struct_termios = undefined;
-
-fn print_anything(prefix_msg: []const u8, thing: anytype, postfix_msg: []const u8) void {
-    std.log.debug(prefix_msg);
-    inline for (std.meta.fields(@TypeOf(thing))) |f| {
-        std.log.debug(f.name ++ " {any}\n", .{@as(f.type, @field(thing, f.name))});
-    }
-    std.log.debug(postfix_msg);
-}
+var original_termios: c.struct_termios = undefined;
 
 /// Perform Init actions
 /// Init Actions:
 ///     - Enter canonical mode
-fn init() !void {
+fn init() void {
     // Get
     _ = c.tcgetattr(c.STDIN_FILENO, &original_termios);
 
     var raw = original_termios;
 
+    _ = io.getStdOut().writer().print("{b}\n", .{raw.c_iflag}) catch {};
+    c.cfmakeraw(&raw);
+    _ = io.getStdOut().writer().print("{b}\n", .{raw.c_iflag}) catch {};
+
     raw.c_cc[c.VMIN] = 1;
     raw.c_cc[c.VTIME] = 0;
-    raw.c_iflag = c.ICANON;
 
     _ = c.tcsetattr(c.STDIN_FILENO, c.TCSAFLUSH, &raw);
 }
 
+fn clean_up() void {
+    _ = c.tcsetattr(c.STDIN_FILENO, c.TCSAFLUSH, &original_termios);
+}
+
 pub fn main() !void {
-    try init();
+    init();
+    defer clean_up();
+
+    const stdin = io.getStdIn().reader();
+    const stdout = io.getStdOut().writer();
+
+    var buffer: [1]u8 = undefined;
+    _ = try stdin.read(&buffer);
+    while (buffer[0] != 'q') {
+        try stdout.print("user input > 0x{x} {c}\n", .{ buffer[0], buffer[0] });
+        _ = try stdin.read(&buffer);
+    }
+
+    try stdout.print("EXITING\n", .{});
 }
